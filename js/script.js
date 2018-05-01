cb397ad158e5fb0203ed5f150949becdc508fcde
mofongo74 <mad.gad.gomez@gmail.com>
2018-05-01 18:35:46 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,10 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  function (categories) {
-    document.querySelector("#main-content")
-      .innerHTML = categories;
-  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -105,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -119,31 +116,16 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-
-
+      chosenCategoryShortName = 'chosenCategoryShortName';
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
+                                                        "randomCategoryShortName",
+                                                        chosenCategoryShortName);
+      
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
-      $ajaxUtils.sendGetRequest(
-        chooseRandomCategory,
-        function (chooseRandomCategory) {
-          var chosenCategoryShortName = 
-            buildCategoriesViewHtml(categories,
-                                    homeHtml,
-                                    chooseRandomCategory);
-          /*
-          var insertProperty = function (string, propName, propValue) {
-          var propToReplace = "{{" + propName + "}}";
-          string = string
-            .replace(new RegExp(propToReplace, "g"), propValue);
-          return string;
-          };
-          */                         
-          var homeHtmlToInsertIntoMainPage = "'{{" + chosenCategoryShortName + "}}'";                     
-          insertHtml("#main-content", homeHtmlToInsertIntoMainPage);  
-        },
-        false);
+      // ....                  
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);  
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
