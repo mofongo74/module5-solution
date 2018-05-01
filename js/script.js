e61c18ec1bb7c6c14a093fa52a71f02bd3d0774e
mofongo74 <mad.gad.gomez@gmail.com>
2018-05-01 13:54:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,9 +83,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  $ajaxUtils.sendGetRequest(
-    buildAndShowHomeHTML,
-    true), // ***** <---- TODO: STEP 1: Substitute [...] ******
+  function (categories) {
+    document.querySelector("#main-content")
+      .innerHTML = categories;
+  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
